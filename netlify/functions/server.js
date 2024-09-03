7ced4f6dd648c6044fbbb47a5c3b30358d146ab2
malaysrivastava <malaypreet2013@gmail.com>
2024-09-04 00:00:30 +0530

    dist-added

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,7 +3,8 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 exports.handler = async (event, context) => {
-    const serverPath = path.resolve(__dirname, '..', '..', 'dist', 'server.js');
+    console.log(__dirname);
+    const serverPath = path.resolve(__dirname, '../../dist/server.js');
 
     // Spawn the node process to run the server.js in the background
     const child = spawn('node', [serverPath], {
