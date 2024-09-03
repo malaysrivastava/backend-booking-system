08936c4d2b9429ebcf7e7a7a85763fb26ac41024
malaysrivastava <malaypreet2013@gmail.com>
2024-09-04 00:03:04 +0530

    dist-added

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,9 +1,20 @@
 // netlify/functions/server.js
 const { spawn } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 exports.handler = async (event, context) => {
-    console.log(__dirname);
+    const currentDir = path.resolve(__dirname);
+
+    // Reading the directory contents
+    const files = fs.readdirSync(currentDir);
+
+    // Logging the directory path and files
+    console.log('Current directory:', currentDir);
+    console.log('Files under __dirname:');
+    files.forEach(file => {
+        console.log(file);
+    });
     const serverPath = path.resolve(__dirname, '../../dist/server.js');
 
     // Spawn the node process to run the server.js in the background
